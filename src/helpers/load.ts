8da4d014ae348dd03ceae1761bedcb2ce1ede602
Ordnay Perez Hernandez <ordnayperez7@gmail.com>
2026-02-04 12:35:18 -0500

    helpers typed

--- src/helpers/load.ts
+++ src/helpers/load.ts
@@ -1,20 +1,24 @@
 import axios from "axios";
+import evts from "../data/events"
 
 type LoadResponse = Promise<{
-    type: string,
-    payload: { dishes: string[] } | { error: unknown }
+    type: "SUCCESS",
+    payload: { dishes: object[], events: object[]}
+} | {
+    type: "ERROR",
+    payload: { error: unknown }
 }>
 
 async function load(url: string): LoadResponse {
     try {
-        let data : string[] = JSON.parse(localStorage.getItem(url) ?? "[]")
+        let data : object[] = JSON.parse(localStorage.getItem(url) ?? "[]")
 
         if(data.length === 0) {
             data = (await axios.get(url)).data
         }
 
         localStorage.setItem(url, JSON.stringify(data))
-        return { type: 'SUCCESS', payload: { dishes: data } }
+        return { type: 'SUCCESS', payload: { dishes: data, events: evts  } }
     } catch(error) {
         return { type: 'ERROR', payload: { error: error } }
     }
